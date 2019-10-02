d9f2b01127377192d1393cf7c6f08f8ab5e1a72c
droneal11015 <droneal11015@gmail.com>
2019-10-02 16:03:09 -0400

    completed args

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(a, b):
+    return a + b
 
 print(f1(1, 2))
 
@@ -12,6 +14,12 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+def f2(*argv):
+    x = 0
+    for arg in argv:
+        x += arg
+    return x
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -28,6 +36,9 @@ print(f2(a))    # Should print 22
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1, num2=1):
+    return num1+num2
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +53,11 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+
+def f4(**kwargs):
+    for arg in kwargs:
+        print(f'key: {arg}, value: {kwargs[arg]}')
+
 
 # Should print
 # key: a, value: 12
@@ -60,4 +76,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
